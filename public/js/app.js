cfab93edc0330393e4df17580c78f7d0c5474d49
mlmng <sany_sky@hotmail.com>
2014-11-04 22:01:54 +0700

    include angular after create table

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
 //app.js
-angular.module(“myapp”,[])
-.controller(‘mainCtrl’ , function($scope) {
-$scope.name = ‘Suthon’;
+angular.module("myApp", [])
+.controller('mainCtrl', function($scope, $http){
+	$scope.books = [];
+	$http.get('/api/book').success(function(data){
+		$scope.books = data;
+	})
 })
