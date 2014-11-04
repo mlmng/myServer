32e80ebc98b1895d97fa79edfe800cae7834d89a
mlmng <sany_sky@hotmail.com>
2014-11-04 16:48:20 +0700

    create angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,5 @@
 //app.js
-angular.module("myApp",[])
-.controller('mainCtrl',function($scope){
-	$scope.books =[];
-	$http.get('/api/book').success(function(data){
-		$scope.books = data;
-	})
+angular.module(“myapp”,[])
+.controller(‘mainCtrl’ , function($scope) {
+$scope.name = ‘Suthon’;
 })
