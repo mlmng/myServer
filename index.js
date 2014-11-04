03e498f754a2de2da82bf77f11ed45d993ed8ff2
mlmng <sany_sky@hotmail.com>
2014-11-04 17:58:34 +0700

    prepare index

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static(__dirname + '/public')); //middle ware 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/api/book', function(req, res){
-	var books =[
+	var books = [
 		{title: 'Angular', price: 800},
 		{title: 'Node.js', price: 600},
-		{title: 'CSS', price: 400}
+		{title: 'CSS', price: 500}
 	];
 	res.send(books);
 })
-	var server = app.listen(3000, function () {
 
-  		console.log("server is running")
-
+var server = app.listen(3000, function () {
+	console.log("server is running")
 })
