a682322e3e5f7a428996c888ebb33ef60e122a04
mlmng <sany_sky@hotmail.com>
2014-11-09 19:56:28 +0700

    commit 11 require mongojs & body-parser

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 //code in server
 var express = require('express')
+var mongojs = require('mongojs')
+var bodyParser = require('body-parser')
 var app = express()
+var db = mongojs('my_server', ['book']);
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/api/book',function(req,res){
-	var books = [
-		{title: 'Hello Kitty', price: 800},
-		{title: 'My melody', price: 600},
-		{title: 'Doraemon', price: 400}
-	];
-	res.send(books);
+	db.book.find({}, function(err, books){
+		res.send(books);
+	});
+	
 })
+
+app.post('/api/book', function(req, res){
+	db.book.insert(req.body, function(err, books){
+			res.send(books);
+	});
+});
 
 var server = app.listen(3000, function () {
 
